a403b05e1c5c506bfce597d7e3a1bd631e18fd22
sjmamani <santiagojmamani@gmail.com>
2020-02-28 17:52:47 -0300

    Dist

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,8 +1,8 @@
 export class Usuario {
   constructor(
     public id: number,
-    public firstName: string,
-    public lastName: string,
+    public first_name: string,
+    public last_name: string,
     public avatar: string
   ) {}
 }
